a1fe8bc72348863305d39e4d5f44ed09144d2585
Gurveer80 <10220@sheridancollege.ca>
2020-06-02 19:06:46 -0400

    first commit

---
+++ src/softwareweek2/Softwareweek2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareweek2;
+
+/**
+ *
+ * @author Gurveer Singh
+ */
+public class Softwareweek2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Software fundamental");
+    }
+    
+}
